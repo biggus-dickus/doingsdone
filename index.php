a02001c810ad0a6149d7fc1e309f0946c624bbc0
MaxM <lynchnost@gmail.com>
2017-08-26 00:04:33 +0300

    Floored 'days_until_deadline'

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $current_ts = strtotime('now midnight'); // текущая метка време
 $date_deadline = date('d. m. Y', $task_deadline_ts);
 
 // в эту переменную запишите кол-во дней до даты задачи
-$days_until_deadline = ($task_deadline_ts - $current_ts) / SECONDS_IN_DAY;
+$days_until_deadline = floor(($task_deadline_ts - $current_ts) / SECONDS_IN_DAY);
 ?>
 
 <!DOCTYPE html>
